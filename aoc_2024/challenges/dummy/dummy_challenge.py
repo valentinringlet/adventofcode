8e6ad5d5d024058c376986b315cef9548c728350
Exodia7 <valentin.ringlet@gmail.com>
2024-11-30 09:51:25 +0100

    Add file reading to Dummy challenge

--- aoc_2024/challenges/dummy/dummy_challenge.py
+++ aoc_2024/challenges/dummy/dummy_challenge.py
@@ -1,7 +1,13 @@
+import os.path
+
 from shared.challenge import Challenge
 
 
 CHALLENGE_ID = "test"
+INPUT_FILE = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)), "dummy_input.txt"
+)
+COMMENT_SYMBOL = "#"
 
 
 class DummyChallenge(Challenge):
@@ -14,4 +20,22 @@ class DummyChallenge(Challenge):
 
     def solve(self):
         print("Some logic will be executed here.")
-        print("Good job reaching this!")
+        print("Good job reaching this!\n")
+
+        # Example logic:
+        with open(INPUT_FILE, "r") as file:
+            challenge_input = file.readlines()
+
+        result = ""
+        for line in challenge_input:
+            if not self._is_comment(line):
+                if line.strip() == "":
+                    result += " "
+                else:
+                    result += line[0]
+
+        print(f"Solution: '{result}'")
+
+    @staticmethod
+    def _is_comment(line: str) -> bool:
+        return line.startswith(COMMENT_SYMBOL)
