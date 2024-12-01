45eef225b3ccf2b885e3516de36b71f7a7d0fee1
Exodia7 <valentin.ringlet@gmail.com>
2024-12-01 14:56:04 +0100

    add solution of today to solved challenges

--- aoc_2024/shared/variables.py
+++ aoc_2024/shared/variables.py
@@ -2,9 +2,12 @@ from typing import Type
 
 from shared.challenge import Challenge
 from challenges.dummy.dummy_challenge import DummyChallenge
+from challenges.day01.challenge_day01 import ChallengeDay01
+
 
 # TODO: update the list below each time a new problem is resolved
 #   --> parts 1 and 2 of a day are 2 different problems
 ALL_SOLVED_CHALLENGES: list[Type[Challenge]] = [
     DummyChallenge,
+    ChallengeDay01,
 ]
