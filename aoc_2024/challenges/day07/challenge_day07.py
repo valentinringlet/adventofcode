4fad99d7b28344ca68c97be60ff2e6e7441447ad
Exodia7 <valentin.ringlet@gmail.com>
2024-12-07 19:49:08 +0100

    solve part 1 of Day 7

--- aoc_2024/challenges/day07/challenge_day07.py
+++ aoc_2024/challenges/day07/challenge_day07.py
@@ -1,3 +1,6 @@
+import os
+import operator
+
 from shared.challenge import Challenge, DaySolutionDTO
 
 
@@ -7,10 +10,59 @@ class ChallengeDay07(Challenge):
         return "Day7"
 
     def _solve(self):
-        # TODO
-        self.set_solution(DaySolutionDTO("not solved yet", "not solved yet"))
+        # 1. read input data
+        input_file = "input_day07.txt"  # "test_input.txt"
+        input_file_path = os.path.join(os.path.dirname(__file__), input_file)
+        all_equations = self._parse_input(input_file_path)
+
+        # 2. solve part 1
+        # 2.1. find the equations that could possibly be true
+        valid_equations = []
+        for equation in all_equations:
+            result_value, factors = equation
+            start_value, remaining_factors = factors[0], factors[1:]
+            if self._can_factors_make_target(
+                start_value, remaining_factors, result_value
+            ):
+                valid_equations.append(equation)
+        # 2.2. compute the solution of part 1
+        solution_part1 = sum(
+            [result_value for result_value, factors in valid_equations]
+        )
+
+        self.set_solution(DaySolutionDTO(str(solution_part1), "not solved yet"))
 
     def _print_solution(self):
         solution = self.get_solution()
         print(f"- part 1: {solution.solution_part1}")
         print(f"- part 2: {solution.solution_part2}")
+
+    @staticmethod
+    def _parse_input(file_path: str) -> list[tuple[int, tuple[int, ...]]]:
+        with open(file_path, "r") as file:
+            all_equations_str = [
+                line.strip().split(":")
+                for line in file.readlines()
+                if line.strip() != ""
+            ]
+            all_equations = [
+                (int(result), tuple([int(num) for num in factors.strip().split()]))
+                for result, factors in all_equations_str
+            ]
+            return all_equations
+
+    def _can_factors_make_target(
+        self, current_value: int, factors: tuple[int, ...], target_value: int
+    ) -> bool:
+        if len(factors) == 0:
+            if current_value == target_value:
+                return True
+            else:
+                return False
+        else:
+            for operation in [operator.add, operator.mul]:
+                if self._can_factors_make_target(
+                    operation(current_value, factors[0]), factors[1:], target_value
+                ):
+                    return True
+            return False
