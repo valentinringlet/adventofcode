5aca07198c7870886178768c1d9e101e381c12c6
Exodia7 <valentin.ringlet@gmail.com>
2024-12-08 10:08:30 +0100

    add base code for Day 8

---
+++ aoc_2024/challenges/day08/challenge_day08.py
@@ -0,0 +1,11 @@
+from shared.challenge import Challenge
+
+
+class ChallengeDay08(Challenge):
+    @classmethod
+    def id(cls) -> str:
+        return "Day8"
+
+    def _solve(self):
+        # TODO
+        self.set_solution("not solved yet", "not solved yet")
--- aoc_2024/shared/variables.py
+++ aoc_2024/shared/variables.py
@@ -9,6 +9,7 @@ from challenges.day04.challenge_day04 import ChallengeDay04
 from challenges.day05.challenge_day05 import ChallengeDay05
 from challenges.day06.challenge_day06 import ChallengeDay06
 from challenges.day07.challenge_day07 import ChallengeDay07
+from challenges.day08.challenge_day08 import ChallengeDay08
 
 
 # TODO: update the list below each time a new problem is resolved
@@ -22,4 +23,5 @@ ALL_SOLVED_CHALLENGES: list[Type[Challenge]] = [
     ChallengeDay05,
     ChallengeDay06,
     ChallengeDay07,
+    ChallengeDay08,
 ]
