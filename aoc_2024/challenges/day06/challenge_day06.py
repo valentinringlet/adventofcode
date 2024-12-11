4153cf33a04887aaf05b03831ce6407a431f78cf
Exodia7 <valentin.ringlet@gmail.com>
2024-12-11 20:59:37 +0100

    add sign that Day 6 and Day 9 are not yet fully solved

--- aoc_2024/challenges/day06/challenge_day06.py
+++ aoc_2024/challenges/day06/challenge_day06.py
@@ -21,7 +21,7 @@ class Direction(Enum):
 class ChallengeDay06(Challenge):
     @classmethod
     def id(cls) -> str:
-        return "Day6"
+        return "Day6-not fully solved"
 
     def _solve(self):
         # 1. read input data
--- aoc_2024/challenges/day09/challenge_day09.py
+++ aoc_2024/challenges/day09/challenge_day09.py
@@ -36,7 +36,7 @@ class FreeSpaceBlock(Block):
 class ChallengeDay09(Challenge):
     @classmethod
     def id(cls) -> str:
-        return "Day9"
+        return "Day9-not fully solved"
 
     def _solve(self):
         input_file_name = "test_input_day09.txt"  # "input_day09.txt"  #
