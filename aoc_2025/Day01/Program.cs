d98b6f2df5fa3495c5bb677b58557d82993bff9f
Exodia7 <valentin.ringlet@gmail.com>
2025-12-03 23:28:50 +0100

    Solved part 2 of Day 1

--- aoc_2025/Day01/Program.cs
+++ aoc_2025/Day01/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Read in the input
 var parentDirectory = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
-var inputFileFullPath = Path.Combine(parentDirectory, "test.txt");
+var inputFileFullPath = Path.Combine(parentDirectory, "input.txt");
 var inputLines = File.ReadAllLines(inputFileFullPath);
 var parsedInstructions = new List<(char Dir, int Value)>();
 foreach (var line in inputLines)
@@ -33,3 +33,34 @@ foreach (var (dir, value) in parsedInstructions)
 }
 
 Console.WriteLine($"PART 1 - The dial pointed at zero {pointedAtZeroTimes} times.");
+
+// Part 2 - How many times does the dial point at zero, including passing over it?
+startPos = 50;
+pointedAtZeroTimes = 0;
+foreach (var (dir, value) in parsedInstructions)
+{
+    var oldStartPos = startPos;
+    if (dir == 'L')
+        startPos -= value;
+    else if (dir == 'R')
+        startPos += value;
+    
+    var oldHundredsDigit = oldStartPos / 100;
+    var newHundredsDigit = startPos / 100;
+    pointedAtZeroTimes += Math.Abs(newHundredsDigit - oldHundredsDigit);
+    
+    if (Math.Sign(oldStartPos) != Math.Sign(startPos) && Math.Sign(oldStartPos) != 0)
+        pointedAtZeroTimes++;
+    // This covers different edge cases:
+    // 1. We passed over zero from positive to negative or negative to positive
+    //    --> then the hundreds digit is 0 for both, so we need to add one more count
+    // 2. We ended exactly at zero from a negative or positive position
+    //    --> then the hundreds digit remains the same (for a number in -99 <= and <= 99), 
+    //        so we need to add one more count
+    // 3. We started exactly at zero and moved away from it
+    //    --> then we should not count it as we already counted it when we ended at zero (see previous case)
+
+    startPos = (startPos % 100 + 100) % 100; // Ensure the result is between 0 and 99 as specified in the assignment
+}
+
+Console.WriteLine($"PART 2 - The dial pointed at zero {pointedAtZeroTimes} times.");
