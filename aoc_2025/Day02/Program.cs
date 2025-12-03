25f7ad63b3055d52aff4ec6571a5a82b062960c5
Exodia7 <valentin.ringlet@gmail.com>
2025-12-03 18:25:29 +0100

    Solve part 2 of Day 2

--- aoc_2025/Day02/Program.cs
+++ aoc_2025/Day02/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MyProject;
+﻿using System.Diagnostics;
+
+namespace MyProject;
 class Program
 {
     static void Main(string[] args)
@@ -23,7 +25,7 @@ class Program
           
           foreach (var i in Enumerable.Range(0, (int)(range[1]-range[0]) +1))
           {
-            if (! IsValidProductId(range[0] + i))
+            if (! IsValidProductIdPart1(range[0] + i))
             {
                 invalidProductIds.Add(range[0] + i);
             }
@@ -32,9 +34,26 @@ class Program
         var sumOfInvalidProductIds = invalidProductIds.Sum();
 
         Console.WriteLine($"PART 1 - The sum of invalid product IDs is {sumOfInvalidProductIds}.");
+
+        // Part 2 - Find all invalid product ID ranges and sum them (new criteria for validity)
+        var invalidProductIdsPart2 = new List<long>();
+        foreach (var range in parsedIdRanges)
+        {
+          
+          foreach (var i in Enumerable.Range(0, (int)(range[1]-range[0]) +1))
+          {
+            if (! IsValidProductIdPart2(range[0] + i))
+            {
+                invalidProductIdsPart2.Add(range[0] + i);
+            }
+          }
+        }
+        var sumOfInvalidProductIdsPart2 = invalidProductIdsPart2.Sum();
+
+        Console.WriteLine($"PART 2 - The sum of invalid product IDs is {sumOfInvalidProductIdsPart2}.");
     }
 
-    public static bool IsValidProductId(long productId)
+    public static bool IsValidProductIdPart1(long productId)
     {
         // An invalid product ID is made of some sequence of digits repeated twice
         // e.g. "55", "6464", "123123"
@@ -44,5 +63,27 @@ class Program
             return true;
         var halfLength = productIdStr.Length / 2;
         return ! (productIdStr[..halfLength] == productIdStr[halfLength..]);
+    }
+
+    public static bool IsValidProductIdPart2(long productId)
+    {
+        // An invalid product ID is made of some sequence of digits repeated twice
+        // e.g. "55", "6464", "123123"
+        
+        var productIdStr = productId.ToString();
+        var repeatingSequenceLength = 0;
+        while (repeatingSequenceLength < productIdStr.Length / 2)
+        {
+            repeatingSequenceLength++;
+            if (productIdStr.Length % repeatingSequenceLength != 0)
+              continue;
+            
+            var sequence = productIdStr.Substring(0, repeatingSequenceLength);
+            var repeatedSequence = string.Concat(Enumerable.Repeat(sequence, productIdStr.Length / repeatingSequenceLength));
+            if (repeatedSequence == productIdStr)
+                return false;
+        }
+
+        return true;
     }
 }
