4497b224d89aa72a4c480d3b4baa9d576065b738
Exodia7 <valentin.ringlet@gmail.com>
2025-12-05 21:15:21 +0100

    Solved part 2 of Day 5

--- aoc_2025/Day05/Program.cs
+++ aoc_2025/Day05/Program.cs
@@ -1,4 +1,8 @@
-﻿// Read in the input
+﻿
+
+var DEBUG = false;
+
+// Read in the input
 var parentDirectory = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
 var inputFileFullPath = Path.Combine(parentDirectory, "input.txt");
 var inputLines = File.ReadAllLines(inputFileFullPath);
@@ -25,6 +29,7 @@ foreach (var ingredientId in availableIngredientIds)
     {
         if (Start <= ingredientId && ingredientId <= End)
         {
+            if (DEBUG) Console.WriteLine($"Ingredient ID {ingredientId} is fresh (in range {Start}-{End})");
             freshIngredientCount++;
             break;
         }
@@ -32,3 +37,51 @@ foreach (var ingredientId in availableIngredientIds)
 }
 
 Console.WriteLine("PART 1 - The number of available ingredients that are fresh is " + freshIngredientCount);
+
+// Part 2 - Find the total number of ingredient IDs that are fresh
+long totalNumberOfFreshIngredients = 0;
+var orderedFreshIngredientIdRanges = freshIngredientIdRanges
+    .OrderBy(range => range.Start)
+    .ToList();
+
+if (DEBUG)
+{
+    Console.WriteLine("###############################################\nOrdered fresh ingredient ID ranges:");
+    foreach (var (Start, End) in orderedFreshIngredientIdRanges)
+    {
+        Console.WriteLine($"  {Start}-{End}");
+    }
+    Console.WriteLine("###############################################");
+}
+
+int currentIdx = 0;
+int nextIdx = 0;
+while (currentIdx < orderedFreshIngredientIdRanges.Count)
+{
+    var (currentStart, currentEnd) = orderedFreshIngredientIdRanges[currentIdx];
+    if (DEBUG) Console.WriteLine($"Processing range {currentStart}-{currentEnd}");
+
+    nextIdx = currentIdx + 1;
+    while (nextIdx < orderedFreshIngredientIdRanges.Count)
+    {
+        var (nextStart, nextEnd) = orderedFreshIngredientIdRanges[nextIdx];
+        if (nextStart <= currentEnd + 1)
+        {
+            // Ranges overlap or are contiguous, merge them
+            currentEnd = Math.Max(currentEnd, nextEnd);
+            if (DEBUG) Console.WriteLine($"  Merging with range {nextStart}-{nextEnd} to form {currentStart}-{currentEnd}");
+            nextIdx++;
+        }
+        else
+        {
+            if (DEBUG) Console.WriteLine($"  No overlap with range {nextStart}-{nextEnd}, moving to next range.");
+            break;
+        }
+    }
+
+    if (DEBUG) Console.WriteLine($"Final merged range {currentStart}-{currentEnd}, adding {(currentEnd - currentStart + 1)} to total.");
+    totalNumberOfFreshIngredients += currentEnd - currentStart + 1;
+    currentIdx = nextIdx;
+}
+
+Console.WriteLine("PART 2 - The total number of fresh ingredient IDs is " + totalNumberOfFreshIngredients);
