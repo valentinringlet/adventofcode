560b98ff07a0a09ead90047d71ab09feb0377a34
Exodia7 <valentin.ringlet@gmail.com>
2025-12-14 13:36:57 +0100

    Refactor part 1 solution to prepare for part 2

--- aoc_2025/Day11/Program.cs
+++ aoc_2025/Day11/Program.cs
@@ -4,16 +4,15 @@ namespace Day11;
 
 class Program
 {
-    private static readonly bool DEBUG = true;
-    private static readonly string StartingDevice = "you";
-    private static readonly string FinalDevice = "out";
+    private static readonly bool UseTestInput = true;
 
     static void Main(string[] args)
     {
-        // Read in the input
+        // Part 1 - Find the number of paths leading from the start to the end
+        // 1.1 - Read in the input
         var parentDirectory = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
-        var inputFileFullPath = Path.Combine(parentDirectory, "input.txt");
-        var deviceConnections = File.ReadAllLines(inputFileFullPath)
+        var inputFileFullPath = Path.Combine(parentDirectory, (UseTestInput ? "test-part1.txt" : "input.txt"));
+        var deviceConnectionsPart1 = File.ReadAllLines(inputFileFullPath)
             .Where(line => !string.IsNullOrWhiteSpace(line))
             .Select(line => {
                 var splitParts = line.Split(':', 2);
@@ -25,24 +24,45 @@ class Program
                 return (sourceDevice, destinationDevices);
             }).ToDictionary();
         
-        if (DEBUG) Console.WriteLine("Parsed " + deviceConnections.Count + " device connections.");
+        Console.WriteLine("Parsed " + deviceConnectionsPart1.Count + " device connections.");
 
-        // Part 1 - Find the number of paths leading from the start to the end
+        // 1.2 - Find the number of paths
+        var startingDevicePart1 = "you";
+        var finalDevicePart1 = "out";
+        var numberOfPathsPart1 = ComputeNumberOfPathsFromStartNodeToEndNode(startingDevicePart1, finalDevicePart1, deviceConnectionsPart1);
+
+        Console.WriteLine("PART 1 - The number of paths from " + startingDevicePart1 + " to " + finalDevicePart1 + " is " + numberOfPathsPart1);
+    }
+
+    /// <summary>
+    /// Computes the number of paths from a start node to an end node in a graph,
+    /// where each node is connected to a list of other nodes.
+    /// </summary>
+    /// <param name="startNode">The start node of the path.</param>
+    /// <param name="endNode">The end node of the path.</param>
+    /// <param name="transitions">A dictionary where the key is a node and the value is a list of nodes that the key node is connected to.</param>
+    /// <param name="forbiddenNodes">An optional list of nodes that should not be traversed.</param>
+    public static int ComputeNumberOfPathsFromStartNodeToEndNode(string startNode, string endNode, Dictionary<string, List<string>> transitions, List<string>? forbiddenNodes = null)
+    {
+        forbiddenNodes ??= [];
+
         var paths = new Queue<List<string>>();
-        paths.Enqueue([StartingDevice]);
+        paths.Enqueue([startNode]);
         var completedPaths = new HashSet<List<string>>();
         while (paths.Count > 0)
         {
             var currentPath = paths.Dequeue();
-            var currentDevice = currentPath[^1];
-            if (currentDevice == FinalDevice)
+            var currentNode = currentPath[^1];
+
+            if (forbiddenNodes.Contains(currentNode)) continue;
+            if (currentNode == endNode)
             {
                 completedPaths.Add(currentPath);
                 continue;
             }
             else
             {
-                var possibleNextDevices = deviceConnections[currentDevice];
+                var possibleNextDevices = transitions[currentNode];
                 foreach (var nextDevice in possibleNextDevices)
                 {
                     // avoid loops
@@ -55,6 +75,6 @@ class Program
             }
         }
 
-        Console.WriteLine("PART 1 - The number of paths from " + StartingDevice + " to " + FinalDevice + " is " + completedPaths.Count);
+        return completedPaths.Count;
     }
 }
